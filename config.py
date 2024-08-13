b823236968bb46bd5ba3146f2a49d02f6cafe583
Romeo RJ <124047324+RRomeo-RJ@users.noreply.github.com>
2024-08-13 19:05:05 +0530

    😎

---
+++ config.py
@@ -0,0 +1,11 @@
+from os import getenv
+
+API_ID = int(getenv("API_ID", "14246641"))
+API_HASH = getenv("API_HASH", "")
+BOT_TOKEN = getenv("BOT_TOKEN", "")
+OWNER_ID = int(getenv("OWNER_ID", ""))
+STRING_SESSION = getenv("STRING_SESSION", "")
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
+ALIVE_PIC = getenv("ALIVE_PIC", "https://telegra.ph/file/e745fdaf1966f228582dc.jpg")
+REPO_URL = getenv("REPO_URL", "https://github.com/RRomeo-RJ/Rmusic")
+BRANCH = getenv("BRANCH", "main")
