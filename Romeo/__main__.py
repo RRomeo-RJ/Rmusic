b8c741d8eddafde2ee71640cb8fe1bd6a6e894fd
R-8768 <141494625+R-8768@users.noreply.github.com>
2024-08-13 14:15:36 +0530

    Update __main__.py

--- Romeo/__main__.py
+++ Romeo/__main__.py
@@ -1,13 +1,14 @@
 import asyncio
 import importlib
 from pyrogram import Client, idle
-from Romeo import client, app
+from Romeo import client, app, call_py
 
 async def start_bot():
     await app.start()
     print("LOG: Founded Bot token Booting..")
     print("USERBOT SUCCESSFULLY STARTED ✅✅")
     await client.start()
+    await call_py.start()
     await idle()
 
 loop = asyncio.get_event_loop()
